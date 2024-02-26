7e91367359dd8617623a2d1fe7265422e139f22f
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2024-02-26 07:14:44 +0700

    feat: update api basemaps

--- src/utils/apis/work/api.ts
+++ src/utils/apis/work/api.ts
@@ -5,22 +5,27 @@ export const listMap: ListMap[] = [
   {
     id: 1,
     title: "Satellite",
-    url: "https://api.maptiler.com/maps/satellite/{z}/{x}/{y}@2x.jpg?key=kLeqcgsjbKwXNhtnJ43J",
+    url: "https://api.maptiler.com/maps/satellite/{z}/{x}/{y}@2x.jpg?key=AW8IuG306IIk8kNdxEw6",
   },
   {
     id: 2,
     title: "Streets",
-    url: "https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}@2x.jpg?key=kLeqcgsjbKwXNhtnJ43J",
+    url: "https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}@2x.jpg?key=AW8IuG306IIk8kNdxEw6",
   },
   {
     id: 3,
     title: "Topographic",
-    url: "https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}@2x.png?key=kLeqcgsjbKwXNhtnJ43J",
+    url: "https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}@2x.png?key=AW8IuG306IIk8kNdxEw6",
+  },
+  {
+    id: 4,
+    title: "Bright",
+    url: "https://api.maptiler.com/maps/backdrop/{z}/{x}/{y}@2x.png?key=AW8IuG306IIk8kNdxEw6",
   },
   // {
-  //   id: 4,
-  //   title: "Bright",
-  //   url: "https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}@2x.png?key=kLeqcgsjbKwXNhtnJ43J",
+  //   id: 5,
+  //   title: "dark",
+  //   url: "https://api.maptiler.com/maps/nl-cartiqo-dark/style.json?key=AW8IuG306IIk8kNdxEw6",
   // },
 ];
 
