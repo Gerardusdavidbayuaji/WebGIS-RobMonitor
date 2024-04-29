7c06ec7d022ced0f78b1cbd407acd16fb54b7076
Gerardusdavid <gerardusdavidbayu@gmail.com>
2024-04-29 16:38:44 +0700

    refactor: change code

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,37 +1,66 @@
-import { Coordinate } from "ol/coordinate";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
+import { useGeographic } from 'ol/proj';
+import { Geometry } from "ol/geom";
+import { Feature } from "ol";
+
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import Map from 'ol/Map';
 import "ol/ol.css";
 
 const MapPlain = () => {
-  const getCoordinate = async (): Promise<Coordinate> => {
-    const apiUrl = 'http://localhost:8080/geoserver/gsdb_simadu/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gsdb_simadu%3Aprovinsi_jateng&maxFeatures=50&outputFormat=application%2Fjson';
+  const [geojsonData, setGeojsonData] = useState(null);
+  const apiUrl = 'http://localhost:8080/geoserver/gsdb_simadu/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gsdb_simadu%3Aprovinsi_jateng&outputFormat=application%2Fjson';
 
-    try {
-      const response = await axios.get(apiUrl);
-      return response.data;
-    } catch (error: any) {
-      throw Error(error.response.data);
-    }
-  };
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getCoordinate();
-        console.log('tes', data);
-      } catch (error: any) {
-        console.log('uppss error guys', error);
+        const response = await axios.get(apiUrl);
+        setGeojsonData(response.data);
+      } catch (error) {
+        console.log('upss, something when wrong', error);
       }
-    }
-
+    };
+    
     fetchData();
   }, []);
+  
+  useEffect(() => {
+    if (geojsonData) {
+      console.log('data jateng', geojsonData);
+      useGeographic();
+      const vectorSource = new VectorSource({
+        features: new GeoJSON().readFeatures(geojsonData) as Feature<Geometry>[],
+      });
+      
+      const vectorLayer = new VectorLayer({
+        source: vectorSource,
+      });
+
+      new Map({
+        target: 'map-container',
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          }),
+          vectorLayer,
+        ],
+        view: new View({
+          center: [110, -7],
+          zoom: 8,
+        }),
+      });
+    }
+  }, [geojsonData]);
 
   return (
-    <>
-      <div id="map-container" className="w-full h-screen z-0 absolute top-0"></div>
-    </>
+    <div id="map-container" className="w-full h-screen z-0 absolute top-0"></div>
   );
 };
 
