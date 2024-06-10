0bb0b32972c75cffd01c2e5c93149a51a0304536
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-06-10 15:54:17 +0700

    refactor: feature insight

--- src/pages/work/insight.tsx
+++ src/pages/work/insight.tsx
@@ -1,5 +1,9 @@
 import Navbar from "@/components/Navbar";
 import avatar from "/assets/avatar.png";
+import lokasi from "/assets/lokasi.jpg";
+import tinggiGenangan from "/assets/tinggi_genangan.jpg";
+import kerusakanBangunan from "/assets/kerusakan_bangunan.jpg";
+import kerugianBangunan from "/assets/kerugian_bangunan.jpg";
 
 const Insight = () => {
   return (
@@ -21,33 +25,76 @@ const Insight = () => {
 
       <div className="flex justify-center">
         <div className="w-3/5 text-white text-justify">
-          <h1>Abstrak</h1>
-          <p className="text-lg leading-relaxed">
+          <p className="text-base mb-10">
             Pemukiman di wilayah pesisir Kecamatan Kraksaan, Probolinggo
             berbatasan langsung dengan Selat Madura dan berpotensi tinggi
             terdampak banjir rob karena pasang tinggi tertinggi menyebabkan
-            sungai meluap. Penelitian ini bertujuan untuk: (1) mengetahui
-            ketinggian genangan banjir rob dan luas area tergenang banjir rob,
-            (2) mengetahui dampak genangan banjir rob terhadap keruskanan
-            bangunan, (3) mengetahui estimasi kerugian bangunan akibat banjir
-            rob. Pemetaan genangan banjir rob menggunakan metode Hloss
-            memanfaatkan data DEM, data citra Sentinel-2A, dan data pasang
-            surut. Perhitungan estimasi kerugian bangunan menggunakan metode
-            Damage and Loss Assessment (DaLA). Hasil penelitian menunjukan bahwa
-            tinggi genangan banjir rob tahun 2021 adalah 2.33 meter dengan
-            kategori bahaya rendah seluas 17.55 ha, kategori bahaya sedang
-            seluas 15,33 ha, kategori bahaya tinggi seluas 12.49 ha. Bangunan
-            terdampak genangan banjir rob pada tahun 2021 kategori bahaya rendah
-            sebanyak 794 unit, kategori bahaya sedang sebanyak 400 unit,
-            kategori bahaya tinggi sebanyak 538 unit, dan total 1732 unit yang
-            mengalami rusak ringan. Kerugian bangunan rumah permanen adalah
-            Rp9,886,758,206, kerugian bangunan perdagangan dan jasa adalah
-            Rp9,688,495,305, kerugian bangunan industri dan gudang
-            Rp16,866,202,723, kerugian bangunan tempat ibadah adalah
-            Rp121,355,451, dan kerugian bangunan kantor pemerintah adalah
-            Rp227,541,470. Total kerugian bangunan terdampak genangan banjir rob
-            di Kecamatan Kraksaan pada tahun 2021 adalah Rp36,790,353,155.
+            sungai meluap.{" "}
+            <span className="text-[#007afc]">
+              Penelitian ini bertujuan untuk: (1){" "}
+              <span className="underline">
+                mengetahui ketinggian genangan banjir rob dan luas area
+                tergenang banjir rob,
+              </span>{" "}
+              (2){" "}
+              <span className="underline">
+                mengetahui dampak genangan banjir rob terhadap keruskanan
+                bangunan,
+              </span>{" "}
+              (3){" "}
+              <span className="underline">
+                mengetahui estimasi kerugian bangunan akibat banjir rob.
+              </span>
+            </span>{" "}
+            Pemetaan genangan banjir rob menggunakan metode Hloss memanfaatkan
+            data DEM, data citra Sentinel-2A, dan data pasang surut. Perhitungan
+            estimasi kerugian bangunan menggunakan metode Damage and Loss
+            Assessment (DaLA). Hasil penelitian menunjukan bahwa tinggi genangan
+            banjir rob tahun 2021 adalah 2.33 meter dengan kategori bahaya
+            rendah seluas 17.55 ha, kategori bahaya sedang seluas 15,33 ha,
+            kategori bahaya tinggi seluas 12.49 ha. Bangunan terdampak genangan
+            banjir rob pada tahun 2021 kategori bahaya rendah sebanyak 794 unit,
+            kategori bahaya sedang sebanyak 400 unit, kategori bahaya tinggi
+            sebanyak 538 unit, dan total 1732 unit yang mengalami rusak ringan.
+            Kerugian bangunan rumah permanen adalah Rp9,886,758,206, kerugian
+            bangunan perdagangan dan jasa adalah Rp9,688,495,305, kerugian
+            bangunan industri dan gudang Rp16,866,202,723, kerugian bangunan
+            tempat ibadah adalah Rp121,355,451, dan kerugian bangunan kantor
+            pemerintah adalah Rp227,541,470. Total kerugian bangunan terdampak
+            genangan banjir rob di Kecamatan Kraksaan pada tahun 2021 adalah
+            Rp36,790,353,155.
           </p>
+          <p className="text-base leading-relaxed mb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium tenetur reprehenderit sunt maxime quibusdam distinctio
+            culpa unde dolor commodi quo voluptate asperiores, placeat, harum
+            voluptas eius. Minima placeat veniam assumenda?
+          </p>
+          <img src={lokasi} alt="avatar" className="mx-auto mb-10" />
+
+          <p className="text-base leading-relaxed mb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium tenetur reprehenderit sunt maxime quibusdam distinctio
+            culpa unde dolor commodi quo voluptate asperiores, placeat, harum
+            voluptas eius. Minima placeat veniam assumenda?
+          </p>
+          <img src={tinggiGenangan} alt="avatar" className="mx-auto mb-10" />
+
+          <p className="text-base leading-relaxed mb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium tenetur reprehenderit sunt maxime quibusdam distinctio
+            culpa unde dolor commodi quo voluptate asperiores, placeat, harum
+            voluptas eius. Minima placeat veniam assumenda?
+          </p>
+          <img src={kerusakanBangunan} alt="avatar" className="mx-auto mb-10" />
+
+          <p className="text-base leading-relaxed mb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium tenetur reprehenderit sunt maxime quibusdam distinctio
+            culpa unde dolor commodi quo voluptate asperiores, placeat, harum
+            voluptas eius. Minima placeat veniam assumenda?
+          </p>
+          <img src={kerugianBangunan} alt="avatar" className="mx-auto mb-10" />
         </div>
       </div>
     </div>
