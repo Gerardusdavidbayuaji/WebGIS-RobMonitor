97079ed15f963035550c566c57cc12b6539811db
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-06-10 15:54:37 +0700

    refactor: feature studio

--- src/pages/work/studio.tsx
+++ src/pages/work/studio.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/components/Navbar";
+
 const Studio = () => {
-  return <div>Studio</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="bg-[#292929] text-white w-full h-screen flex items-center justify-center">
+        <p>Upss, under maintenance bro</p>
+      </div>
+    </div>
+  );
 };
 
 export default Studio;
