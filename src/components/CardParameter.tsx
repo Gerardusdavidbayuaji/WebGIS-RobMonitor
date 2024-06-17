d835389e2a04bee249ef69e3ec61f7e1c51a3ce9
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-06-17 19:13:06 +0700

    refatore: parameter

--- src/components/CardParameter.tsx
+++ src/components/CardParameter.tsx
@@ -1,4 +1,4 @@
-import { Eye, TableProperties, Map } from "lucide-react";
+import { Eye, Waypoints, Map } from "lucide-react";
 
 interface CardParametersProps {
   title: string;
@@ -9,25 +9,29 @@ interface CardParametersProps {
 const CardParameter = (props: CardParametersProps) => {
   const { title, isActive, onToggle } = props;
   return (
-    <div className="w-full max-w-64 p-2 rounded-lg shadow-md bg-white z-10">
-      <h2 className="text-base font-medium mb-4">{title}</h2>
-      <div className="my-1 flex gap-2">
-        <button className="bg-[#e6eefc] hover:bg-[#5f6e8c] rounded-md">
-          <Eye className="my-1 mx-2" />
-        </button>
-        <button className="bg-[#e6eefc] hover:bg-[#5f6e8c] rounded-md">
-          <TableProperties className="my-1 mx-2" />
-        </button>
-        <button
-          className={`rounded-md ${
-            isActive
-              ? "bg-blue-500 text-white"
-              : "bg-[#e6eefc] hover:bg-[#5f6e8c]"
-          }`}
-          onClick={() => onToggle(title)}
-        >
-          <Map className="my-1 mx-2" />
-        </button>
+    <div className="w-full max-w-64 p-2 rounded-lg shadow-md bg-[#292929] border border-[#94949469] z-10">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h2 className="text-bas text-[#ff6d1f] font-semimedium mb-4">
+          {title}
+        </h2>
+        <div className="my-1 flex gap-2">
+          <button className="bg-[#e3e1dc98] hover:bg-[#94949469] rounded-md">
+            <Eye className="my-1 mx-2" />
+          </button>
+          <button className="bg-[#e3e1dc98] hover:bg-[#94949469] rounded-md">
+            <Waypoints className="my-1 mx-2" />
+          </button>
+          <button
+            className={`rounded-md ${
+              isActive
+                ? "bg-[#ff6d1f] text-white"
+                : "bg-[#e3e1dc98] hover:bg-[#94949469]"
+            }`}
+            onClick={() => onToggle(title)}
+          >
+            <Map className="my-1 mx-2" />
+          </button>
+        </div>
       </div>
     </div>
   );
