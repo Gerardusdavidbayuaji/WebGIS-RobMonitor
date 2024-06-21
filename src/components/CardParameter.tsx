83ec1b240ee902767b61413eeb4f17b5de8c7ad7
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-06-21 15:30:36 +0700

    ref: update cardparameter feature

--- src/components/CardParameter.tsx
+++ src/components/CardParameter.tsx
@@ -4,18 +4,31 @@ interface CardParametersProps {
   title: string;
   isActive: boolean;
   onToggle: (param: string) => void;
+  onEyeClick: (param: string) => void;
 }
 
 const CardParameter = (props: CardParametersProps) => {
-  const { title, isActive, onToggle } = props;
+  const { title, isActive, onToggle, onEyeClick } = props;
+
+  const handleEyeclick = () => {
+    onEyeClick(title);
+  };
+
   return (
     <div className="w-full max-w-64 p-2 rounded-lg shadow-md bg-[#292929] border border-[#94949469] z-10">
       <div className="flex flex-col items-center justify-center h-full">
-        <h2 className="text-bas text-[#ff6d1f] font-semimedium mb-4">
+        <h2 className="text-base text-[#ff6d1f] font-semimedium mb-4">
           {title}
         </h2>
         <div className="my-1 flex gap-2">
-          <button className="bg-[#e3e1dc98] hover:bg-[#94949469] rounded-md">
+          <button
+            className={`rounded-md ${
+              isActive
+                ? "bg-[#ff6d1f] text-white"
+                : "bg-[#e3e1dc98] hover:bg-[#94949469]"
+            }`}
+            onClick={handleEyeclick}
+          >
             <Eye className="my-1 mx-2" />
           </button>
           <button className="bg-[#e3e1dc98] hover:bg-[#94949469] rounded-md">
