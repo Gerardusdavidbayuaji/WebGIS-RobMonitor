87c19e50c68d60762a2eec2c122f3873ccc990ce
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-06-21 15:30:09 +0700

    ref: update dashboard feature

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardParameter from "@/components/CardParameter";
 import CardBaseMap from "@/components/CardBaseMap";
 import Basemap from "@/components/Basemap";
 import Sidebar from "@/components/Sidebar";
+import { getGenanganRob } from "@/utils/apis/work";
 
 const Dashboard = () => {
   const [activateParam, setActivateParam] = useState<string | null>(null);
+  const [data, setData] = useState<any>(null);
+  const [zoomToData, setZoomToData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getGenanganRob();
+        setData(result);
+        if (activateParam === "Bahaya Rob Rendah") {
+          setZoomToData(result);
+        }
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      }
+    };
+
+    if (activateParam == "Bahaya Rob Rendah") {
+      fetchData();
+    } else {
+      setData(null);
+      setZoomToData(null);
+    }
+  }, [activateParam]);
 
   const toggleSideBar = (param: string) => {
     setActivateParam(activateParam === param ? null : param);
+  };
+
+  const handleZoomToData = (param: string) => {
+    if (param === "Bahaya Rob Rendah") {
+      setZoomToData(data);
+    }
   };
 
   const parameters = [
@@ -19,9 +49,10 @@ const Dashboard = () => {
     "Batas Kecamatan",
     "Sungai",
   ];
+
   return (
     <div className="relative min-h-screen">
-      <Basemap />
+      <Basemap data={data} zoomToData={zoomToData} />
       <div className="fixed bottom-0 left-0 right-0 p-4 z-10 mb-2">
         <div className="flex space-x-4 overflow-x-auto">
           {parameters.map((param) => (
@@ -30,6 +61,7 @@ const Dashboard = () => {
               title={param}
               isActive={activateParam === param}
               onToggle={toggleSideBar}
+              onEyeClick={handleZoomToData}
             />
           ))}
         </div>
